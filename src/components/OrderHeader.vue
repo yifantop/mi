baa0ced0aa812cd3307447d696f4add0223fe1fd
erfan <erfanvv@163.com>
2020-05-24 19:33:01 +0800

    4-7路由封装

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,12 +1,9 @@
 <template>
-  <div>
-    order-confirm
-  </div>
+  <div>order-header</div>
 </template>
 
 <script>
 export default {
-  name: "order-confirm",
+  name: "order-header",
 };
 </script>
->
